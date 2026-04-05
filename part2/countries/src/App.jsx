1a60ba0fcdd9fa9cbcf6d0c4367468d26dc959ba
AdiKuterman <adikuterman@mail.tau.ac.il>
2026-04-05 12:08:21 +0300

    ex_2.18

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Filter from './Filter'
+import Countries from './Countries'
+
+const App = () => {
+  const [allCountries, setAllCountries] = useState([])
+  const [filter, setFilter] = useState('')
+
+  const countriesToShow = allCountries.filter(country => 
+    country.name.common.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  const handleFilterChange = (event) => setFilter(event.target.value)
+
+  useEffect(() => {
+    axios
+      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setAllCountries(response.data)
+      })
+  }, [])
+
+  return (
+    <div>
+      <Filter value={filter} onChange={handleFilterChange} />
+      <Countries countriesToShow={countriesToShow}/>
+    </div>
+  )
+}
+
+export default App
---
+++ part2/countries/src/Countries.jsx
@@ -0,0 +1,44 @@
+const Countries = ({countriesToShow}) => {
+    if (countriesToShow.length > 10) {
+        return (
+            <div>Too many matches, specify another filter</div>
+        )
+    }
+
+    if (countriesToShow.length > 1) {
+        return (
+            <div>
+            {countriesToShow.map(country => 
+            <div key={country.cca3}>
+                {country.name.common}
+            </div>
+            )}
+            </div>
+        )
+    }
+
+    if (countriesToShow.length === 1) {
+        const country = countriesToShow[0]
+        return (
+            <div>
+                <h1>{country.name.common}</h1>
+                <p>Capital {country.capital}</p>
+                <p>Area {country.area}</p>
+                <h3>Languages</h3>
+                <ul>
+                {Object.values(country.languages).map(lang => (
+                    <li key={lang}>{lang}</li>
+                ))}
+                </ul>
+                <img 
+                    src={country.flags.png} 
+                    alt={`Flag of ${country.name.common}`} 
+                    width="150" 
+                />
+            </div>
+        )
+    }
+
+    return null
+}
+export default Countries
